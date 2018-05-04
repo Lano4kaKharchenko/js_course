ebe167277ee42dcaf326bde24eda8948568d330f
LanochkaKharchenko <lano4ka44@gmail.com>
2018-05-04 20:47:38 +0300

    modifined

--- lesson_10/index.js
+++ lesson_10/index.js
@@ -2,7 +2,7 @@ let myAge = 21;
 let year;
 
 
-if (myAge % 100 == 0 || ((myAge % 100 >=11 && myAge % 100 <= 19) || (myAge % 10 >= 5)))
+if (myAge % 10 == 0 || ((myAge % 100 >=11 && myAge % 100 <= 19) || (myAge % 10 >= 5)))
 {year = "лет";}
 	else if (myAge % 10 == 1) {
 	year = "год";
