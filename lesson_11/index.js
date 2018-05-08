0c9a6d320d10596c5f6f8172a67d0c7d85a6c6f3
LanochkaKharchenko <lano4ka44@gmail.com>
2018-05-08 14:27:22 +0300

    les_11

---
+++ lesson_11/index.js
@@ -0,0 +1,29 @@
+let a  = 9;
+let b = 2;
+let operation = "pow";
+
+switch(operation) {
+case  "add":
+console.log(a + b);
+break;
+
+case "mult":
+console.log(a * b);
+break;
+
+case  "sub":
+console.log(a - b);
+break;
+
+case "div":
+console.log(a / b);
+break;
+  
+case "pow":
+console.log(Math.pow(a, b));
+break;
+
+default:
+console.log ('такой операции нет');
+break;
+}
